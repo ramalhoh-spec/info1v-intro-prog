e8628ff9bbb0786041fb50420d701cc09f9b1d70
ramalhoh-spec <ramalho.h@escolar.ifrn.edu.br>
2025-09-10 17:54:10 -0300

    Create questao3.py
    fiz um programa que le 4 notas e faz a média delas

---
+++ questao3.py
@@ -0,0 +1,7 @@
+n1 = int(input("Primeira nota:"))
+n2 = int(input("Segunda nota:"))
+n3 = int(input("Terceira nota:"))
+n4 = int(input("Quarta nota:"))
+
+média = (n1 + n2 + n3 + n4)/4
+print(média)
